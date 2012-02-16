018fcc3241d7d1d31f7b734b6c2cfc113a24c973
Pierre-Alexandre Meyer <pierre@ning.com>
2012-02-15 17:38:26 -0800

    html: add index.html skeleton
    Signed-off-by: Pierre-Alexandre Meyer <pierre@ning.com>

--- js/ui.js
+++ js/ui.js
@@ -2,6 +2,37 @@
  * Initialize the whole UI
  */
 function initializeUI() {
+    // See http://bugs.jquery.com/ticket/8338 - this is required for the Ajax feedback functions
+    jQuery.ajaxPrefilter(function(options) {
+        options.global = true;
+    });
+
+    // Global variables
+    window.jansky = {
+        // The actual data
+        timeseries: [],
+        // Scale factors for all graphs (i.e. mapping between graph number and scale factor)
+        scales: {},
+        // When reading a permalink, this variable tells us how many graphs have to be retrieved - this is used for the progress bar
+        permalink_loads: 0,
+        // When reading a permalink, this variable tells us how many graphs have been retrieved already - this is used for the progress bar
+        permalink_loads_done: 0,
+        // The actual graph object
+        graph: null,
+        // Settings for the graph, as defined by the left "Rendering" buttons
+        graph_settings: {
+            interpolation: 'cardinal',
+            offset: 'zero',
+            renderer: 'line'
+        },
+        // Permalink temporary data for the graphs.
+        graph_permalink: [],
+        // The graph palette, used to generate the colors for the different graphs
+        graph_palette: new Rickshaw.Color.Palette({ scheme: 'colorwheel' }),
+        // Global lock used to serialize the rendering of graphs
+        graph_lock: 0
+    };
+
     // Hide the progress bar (used when loading permalinks)
     $('#loadingDiv').hide();
 
